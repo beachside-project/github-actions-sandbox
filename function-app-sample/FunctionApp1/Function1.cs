3452009427431d5e616d2e8bb124117d1026f71e
Atsushi YOKOHAMA <brave.mont.saint.micheal@gmail.com>
2021-09-02 22:11:19 +0900

    Update Function1.cs

--- function-app-sample/FunctionApp1/Function1.cs
+++ function-app-sample/FunctionApp1/Function1.cs
@@ -19,7 +19,7 @@ namespace FunctionApp1
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string name = req.Query["name"] + "V2";
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
